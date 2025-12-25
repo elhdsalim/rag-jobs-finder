ba3f72cefbed42fca6dbafcda140c78473585cf3
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-25 01:08:32 +0100

    Added basic types for dataforseo

---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+export type Task = {
+    id: string;
+    status_code: number;
+    status_message: string;
+    time: string;
+    cost: number;
+    result_count: number;
+    path: (string | number)[];
+    data: Record<string, any>[];
+    result: any[] | null;
+};
+
+export type MinimalJobData = {
+    job_id : string;
+    title: string;
+    location: string;
+    source_url: string;
+};
+
+export type EnrichedJobData = MinimalJobData & {
+  employer_name?: string;
+  source_name?: string;
+  contract_type?: string;
+  salary?: string | null;
+  timestamp?: string | null;
+  employer_url : string | null;
+};
