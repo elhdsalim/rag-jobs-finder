2281c9eeba589eee7c9967837f5662464bcf2bf5
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-25 18:40:12 +0100

    Basic call to save jobs data in sqlite db

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import "./env";
 
-import * as fs from 'fs';
+import { promises as fs } from "fs";
+
 import { enrichJobs, fetchJobs } from "./pipeline/fetchJobs";
+import { initDb, saveJobs } from "./db/db";
 
 async function main() {
+    const db = await initDb();
     const { minimalJobs, tasks } = await fetchJobs();
     const enrichedJobs = enrichJobs(minimalJobs, tasks);
 
-    await fs.promises.writeFile("./data.json",JSON.stringify(enrichedJobs, null, 2),"utf-8");
+    await fs.writeFile("./data.json",JSON.stringify(enrichedJobs, null, 2),"utf-8");
+    await saveJobs(db, enrichedJobs);
+
+    console.log(`${enrichedJobs.length} jobs saved to database`);
+
 }
 
 main()
