322e4eef522973a166d0144bf6450433228f3059
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-25 01:49:08 +0100

    Basic usage of the fetchJob method

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as fs from 'fs';
-import { fetchJobs } from "./pipeline/fetchJobs";
+import { enrichJobs, fetchJobs } from "./pipeline/fetchJobs";
 
 
 async function main() {
-    const jobs = await fetchJobs();
-    
-    await fs.promises.writeFile( "./data.json", JSON.stringify(jobs, null, 2), "utf-8");
+    const { minimalJobs, tasks } = await fetchJobs();
+
+    const enrichedJobs = enrichJobs(minimalJobs, tasks);
+
+    await fs.promises.writeFile("./data.json",JSON.stringify(enrichedJobs, null, 2),"utf-8");
 }
 
 main()
