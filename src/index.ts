76ac852cd4b730b4b32f6c03ea08daced7d8a2ba
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-25 01:56:18 +0100

    Added basic use of enrich job functions / fixed env data

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import "./env";
+
 import * as fs from 'fs';
 import { enrichJobs, fetchJobs } from "./pipeline/fetchJobs";
 
-
 async function main() {
     const { minimalJobs, tasks } = await fetchJobs();
-
     const enrichedJobs = enrichJobs(minimalJobs, tasks);
 
     await fs.promises.writeFile("./data.json",JSON.stringify(enrichedJobs, null, 2),"utf-8");
