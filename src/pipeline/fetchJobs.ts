ac1dc8a454df92a1b8aaf037344499ce76f6f1de
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-25 01:55:55 +0100

    Added enriching job functions

--- src/pipeline/fetchJobs.ts
+++ src/pipeline/fetchJobs.ts
@@ -1,13 +1,16 @@
 import { getJobsOffers, waitForAllTasks } from "../services/dataforseo";
-import { MinimalJobData, Task } from "../types/types";
+import { EnrichedJobData, MinimalJobData, Task } from "../types/types";
 
-export async function fetchJobs(): Promise<MinimalJobData[]> {
+export async function fetchJobs(): Promise<{ minimalJobs: MinimalJobData[]; tasks: Task[]; }> {
     const taskIds = await getJobsOffers();
     const tasks = await waitForAllTasks(taskIds);
 
-    const minimalJobData = extractMinimalJobsData(tasks);
+    const minimalJobs = extractMinimalJobsData(tasks);
 
-    return minimalJobData;
+    return {
+        minimalJobs,
+        tasks
+    };
 }
 
 function extractMinimalJobsData(tasks: Task[]): MinimalJobData[] {
@@ -33,4 +36,38 @@ function extractMinimalJobsData(tasks: Task[]): MinimalJobData[] {
     }
 
     return jobs;
+}
+
+function findEnrichedJob(jobId: string, tasks: Task[]) {
+    for (const task of tasks) {
+        if (!task.result || task.result.length === 0) continue;
+
+        for (const item of task.result[0].items) {
+            if (item.job_id === jobId) {
+                return item;
+            }
+        }
+    }
+
+    return null;
+}
+
+export function enrichJobs(minimalJobs: MinimalJobData[], tasks: Task[]): EnrichedJobData[] {
+    const enrichedJobs: EnrichedJobData[] = [];
+
+    for (const job of minimalJobs) {
+        const enrichedJob = findEnrichedJob(job.job_id, tasks);
+
+        enrichedJobs.push({
+            ...job,
+            employer_name: enrichedJob?.employer_name,
+            source_name: enrichedJob?.source_name,
+            contract_type: enrichedJob?.contract_type,
+            salary: enrichedJob?.salary ?? null,
+            timestamp: enrichedJob?.timestamp ?? null,
+            employer_url: enrichedJob?.employer_url ?? null,
+        });
+    }
+
+    return enrichedJobs;
 }
