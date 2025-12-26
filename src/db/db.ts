b8b2e7a9a2378d8613988532b733867ef989e1c9
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-26 19:37:24 +0100

    Fixed syntax error

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,16 +3,16 @@ import { open, Database } from "sqlite";
 import { EnrichedJobData } from "../types/types";
 
 export async function openDb(): Promise<Database> {
-    return open({
-        filename: "./jobs.db",
-        driver: sqlite3.Database
-    });
+  return open({
+    filename: "./jobs.db",
+    driver: sqlite3.Database
+  });
 }
 
 export async function initDb(): Promise<Database> {
-    const db = await openDb();
+  const db = await openDb();
 
-    await db.exec(`
+  await db.exec(`
     CREATE TABLE IF NOT EXISTS jobs (
       job_id TEXT PRIMARY KEY,
       title TEXT NOT NULL,
@@ -26,53 +26,80 @@ export async function initDb(): Promise<Database> {
       timestamp TEXT,
       employer_url TEXT,
 
+      -- NULL at the beginning, we will ask to an IA to fill these fields
+      role TEXT, -- dev developer ? software engineer ?
+      level TEXT, -- senior ? we don't care ?
+      stack TEXT, -- react?node?vue?js?python?etc...
+      is_tech INTEGER, -- to avoid jobs that are not dev related (for example business analyst or whatever)
+      classified_at TEXT, -- date of classification
+
       created_at TEXT NOT NULL,
       updated_at TEXT NOT NULL
     );
   `);
 
-    return db;
+  return db;
 }
 
 export async function saveJobs(db: Database, jobs: EnrichedJobData[]) {
-    const now = new Date().toISOString();
+  const now = new Date().toISOString();
 
-    const stmt = await db.prepare(`
-    INSERT INTO jobs (
-      job_id, title, location, source_url,
-      employer_name, source_name, contract_type,
-      salary, timestamp, employer_url,
-      created_at, updated_at
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ON CONFLICT(job_id) DO UPDATE SET
-      title = excluded.title,
-      location = excluded.location,
-      source_url = excluded.source_url,
-      employer_name = excluded.employer_name,
-      source_name = excluded.source_name,
-      contract_type = excluded.contract_type,
-      salary = excluded.salary,
-      timestamp = excluded.timestamp,
-      employer_url = excluded.employer_url,
-      updated_at = excluded.updated_at
-  `);
+  const stmt = await db.prepare(`
+      INSERT INTO jobs (
+        job_id,
+        title,
+        location,
+        source_url,
+        employer_name,
+        source_name,
+        contract_type,
+        salary,
+        timestamp,
+        employer_url,
+        role,
+        level,
+        stack,
+        is_tech,
+        classified_at,
+        created_at,
+        updated_at
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) -- index of each attributes
+      ON CONFLICT(job_id) DO UPDATE SET
+        title = excluded.title,
+        location = excluded.location,
+        source_url = excluded.source_url,
+        employer_name = excluded.employer_name,
+        source_name = excluded.source_name,
+        contract_type = excluded.contract_type,
+        salary = excluded.salary,
+        timestamp = excluded.timestamp,
+        employer_url = excluded.employer_url,
+        updated_at = excluded.updated_at
+    `);
 
-    for (const job of jobs) {
-        await stmt.run(
-            job.job_id,
-            job.title,
-            job.location,
-            job.source_url,
-            job.employer_name ?? null,
-            job.source_name ?? null,
-            job.contract_type ?? null,
-            job.salary ?? null,
-            job.timestamp ?? null,
-            job.employer_url ?? null,
-            now,
-            now
-        );
-    }
+  for (const job of jobs) {
+    await stmt.run(
+      job.job_id,
+      job.title,
+      job.location,
+      job.source_url,
+      job.employer_name ?? null,
+      job.source_name ?? null,
+      job.contract_type ?? null,
+      job.salary ?? null,
+      job.timestamp ?? null,
+      job.employer_url ?? null,
 
-    await stmt.finalize();
+      null, // role
+      null, // level
+      null, // stack
+      null, // is_tech
+      null, // classified_at
+
+      now,
+      now
+    );
+  }
+
+  await stmt.finalize();
 }
