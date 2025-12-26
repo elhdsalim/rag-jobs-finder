4d454ebf69cbcd7cea9735600e0c086430957c6c
slmhd <salim.elhadad.etu@univ-lille.fr>
2025-12-26 19:37:50 +0100

    Added classification constants

---
+++ src/rag/classification_constants.ts
@@ -0,0 +1,36 @@
+
+export const ROLES = [
+    "frontend",
+    "backend",
+    "fullstack",
+    "mobile",
+    "data",
+    "devops",
+    "qa",
+    "unknown"
+];
+
+export const LEVELS = [
+    "junior",
+    "mid",
+    "senior",
+    "lead",
+    "unknown"
+];
+
+export const STACK = [
+    "javascript",
+    "typescript",
+    "react",
+    "angular",
+    "vue",
+    "node",
+    "java",
+    "python",
+    "php",
+    "go",
+    ".net",
+    "c",
+    "c#",
+    "rust"
+];
